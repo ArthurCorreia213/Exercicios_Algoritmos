19e6248a8307fefc7027fb91235c9dd503d59ce3
ArthurCorreia213 <arthurcnascimento132@gmail.com>
2025-10-02 15:51:03 -0300

    correções exercicios 21 e 23 modularizados

--- lote1.2/lt01_estseq23.py
+++ lote1.2/lt01_estseq23.py
@@ -1,6 +1,20 @@
 #Receba 3 valores obrigatoriamente em ordem crescente e um 4º valor não necessáriamente em ordem. Mostre todos os números em ordem crescente
 #Arthur Correia
-def organizador_de_nums(valor_seq1,valor_seq2,valor_seq3,valor_outro):
+
+def organizador_de_nums():
+    valor_seq1 = int(input("Digite o primeiro valor (em ordem):"))
+    valor_seq2 = int(input("Digite o segundo valor (em ordem):"))
+
+    while valor_seq2<valor_seq1:
+        valor_seq2 = int(input("Digite o segundo valor (em ordem):"))
+
+    valor_seq3 = int(input("Digite o terceiro valor (em ordem): "))
+
+    while valor_seq3<valor_seq2:
+        valor_seq3 = int(input("Digite o terceiro valor (em ordem):"))
+
+
+    valor_outro = int(input("Digite um valor qualquer: "))
 
     if valor_outro>valor_seq3:
         print(valor_seq1,valor_seq2,valor_seq3,valor_outro)
@@ -14,19 +28,4 @@ def organizador_de_nums(valor_seq1,valor_seq2,valor_seq3,valor_outro):
     else:
         print(valor_outro, valor_seq1, valor_seq2, valor_seq3)
 
-        
-valor_1 = int(input("Digite o primeiro valor (em ordem):"))
-valor_2 = int(input("Digite o segundo valor (em ordem):"))
-
-while valor_2<valor_1:
-    valor_2 = int(input("Digite o segundo valor (em ordem):"))
-
-valor_3 = int(input("Digite o terceiro valor (em ordem): "))
-
-while valor_3<valor_2:
-    valor_3 = int(input("Digite o terceiro valor (em ordem):"))
-
-
-valor_4 = int(input("Digite um valor qualquer: "))
-
-organizador_de_nums(valor_1, valor_2, valor_3, valor_4)
+organizador_de_nums()
